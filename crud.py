faf8b4e74b2b040e2fda30919020feb8952c29eb
Luis <Luiseduardo493333@gmail.com>
2025-09-30 11:20:29 -0300

    Função listar e atualizar alunos

--- crud.py
+++ crud.py
@@ -16,3 +16,34 @@ def criar_aluno(nome, idade):
             conexao.close()
 
 criar_aluno("Luis Eduardo", 17)
+
+def listar_aluno(nome, idade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "SELECT * FROM alunos ORDER BY id"
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao listar {erro}")
+            return[]
+        finally:
+            cursor.close()
+            conexao.close()
+
+
+def listar_aluno(id_aluno, nova_idade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "UPDATE alunos SET idade= %s WHERE id = %s",
+                (nova_idade, id_aluno)
+            )
+        except Exception as erro:
+            print(f"Erro ao atualizar um aluno:  {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
